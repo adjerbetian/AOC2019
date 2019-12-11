fdf33fa165dd7831e2841678ed5c111fdbe37064
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-12-11 09:50:55 +0100

    day2 part 1

--- src/day2/TreatOpcodeKtTest.kt
+++ src/day2/TreatOpcodeKtTest.kt
@@ -12,4 +12,31 @@ internal class TreatOpcodeKtTest {
 
         assertEquals(intArrayOf(2, 0, 0, 0, 99).joinToString(), program.joinToString())
     }
+
+    @Test
+    fun simpleMultiplication() {
+        val program = intArrayOf(2, 3, 0, 3, 99);
+
+        treatOpcode(program, 0)
+
+        assertEquals(intArrayOf(2, 3, 0, 6, 99).joinToString(), program.joinToString())
+    }
+
+    @Test
+    fun simpleCombination() {
+        val program = intArrayOf(2, 4, 4, 5, 99, 0);
+
+        treatOpcode(program, 0)
+
+        assertEquals(intArrayOf(2, 4, 4, 5, 99, 9801).joinToString(), program.joinToString())
+    }
+
+    @Test
+    fun complexCombination() {
+        val program = intArrayOf(1, 1, 1, 4, 99, 5, 6, 0, 99);
+
+        treatOpcode(program, 0)
+
+        assertEquals(intArrayOf(30, 1, 1, 4, 2, 5, 6, 0, 99).joinToString(), program.joinToString())
+    }
 }
--- src/day2/treatOpcode.kt
+++ src/day2/treatOpcode.kt
@@ -1,4 +1,18 @@
 package day2
 
+const val ADD = 1;
+const val MULT = 2;
+const val END = 99;
+
+
 fun treatOpcode(program: IntArray, position: Int) {
+    if (program[position] == END) return;
+
+    if (program[position] == ADD) {
+        program[program[position + 3]] = program[program[position + 1]] + program[program[position + 2]];
+    } else if (program[position] == MULT) {
+        program[program[position + 3]] = program[program[position + 1]] * program[program[position + 2]];
+    }
+
+    return treatOpcode(program, position + 4);
 }
