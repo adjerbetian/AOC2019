cf18ed643ac68eda75807813a2818169e57254b0
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-12-11 20:58:40 +0100

    finished day2

--- src/day2/main.kt
+++ src/day2/main.kt
@@ -5,10 +5,15 @@ import java.io.File
 fun main() {
     val program = readInput("src/day2/input.txt")
 
-    program[1] = 12
-    program[2] = 2
-    treatOpcode(program, 0)
-    println(program[0])
+    val memory = program.clone()
+    memory[1] = 12
+    memory[2] = 2
+    treatOpcode(memory)
+    println("First result: ${memory[0]}")
+
+
+    val (noun, verb) = findNounAndVerbFor(program, 19690720)
+    println("Second result: ${100 * noun + verb}")
 }
 
 fun readInput(path: String) = File(path).readText(Charsets.UTF_8).split(",").map { it.toInt() }.toIntArray()
