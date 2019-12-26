9b0181021fc90e0e63d4738035d933ff6da14352
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-12-27 00:44:16 +0100

    ShipMap

--- src/day15/PathGame.kt
+++ src/day15/PathGame.kt
@@ -3,13 +3,11 @@ package day15
 import intCode.IntCodeComputer
 
 class PathGame(val computer: IntCodeComputer) {
-    val map = HashMap<Position, Tile>()
-    private val distances = HashMap<Position, Int>()
-    private val robot = RepairDroid()
+    private val droid = RepairDroid()
+    private val tileMap = ShipMap(droid)
 
     init {
-        distances[robot.position] = 0
-        map[robot.position] = PATH
+        tileMap[droid.position] = PATH
         computer.outputFunction = fun(statusCode: Long) {
             val tile = when (statusCode.toInt()) {
                 0 -> WALL
@@ -31,72 +29,28 @@ class PathGame(val computer: IntCodeComputer) {
     }
 
     private fun treatTile(tile: Tile) {
-        map[robot.getNextPosition()] = tile
+        tileMap[droid.getNextPosition()] = tile
 
         when (tile) {
-            WALL -> robot.turn()
-            else -> {
-                robot.stepForward()
-                distances[robot.position] = robot.steps
-            }
+            WALL -> droid.turn()
+            else -> droid.stepForward()
         }
 
         if (tile is OXYGEN) throw OxygenFound()
-        if (robot.position == Position(0, 0) && robot.direction == NORTH) throw BackToStart()
+        if (droid.position == Position(0, 0) && droid.direction == NORTH) throw BackToStart()
 
         explore()
     }
 
     private fun explore() {
-        computer.addInput(robot.direction.code)
+        computer.addInput(droid.direction.code)
     }
 
-    private fun minX() = map.keys.map { p -> p.x }.min()!!
-    private fun maxX() = map.keys.map { p -> p.x }.max()!!
-    private fun minY() = map.keys.map { p -> p.y }.min()!!
-    private fun maxY() = map.keys.map { p -> p.y }.max()!!
+    fun printMap() = println(toString())
 
-    fun printMap() {
-        println(toString())
-    }
+    override fun toString() = tileMap.toString()
 
-    override fun toString(): String {
-        val path = getPathToOxygen()
-        var result = ""
-        for (y in maxY() downTo minY()) {
-            for (x in minX()..maxX()) {
-                result += stringifyPosition(Position(x, y), path)
-            }
-            result += "\n"
-        }
-        return result
-    }
-
-    private fun stringifyPosition(position: Position, path: List<Position> = emptyList()): String {
-        val colorizer = Colorizer()
-        return when {
-            map[position] == null -> " "
-            map[position] is WALL -> colorizer.colorize("▯", colorizer.BLACK)
-            map[position] is OXYGEN -> colorizer.colorize("●", colorizer.BLUE)
-            position == robot.position -> colorizer.colorize(robot.direction.toString(), colorizer.RED)
-            position == Position(0, 0) -> colorizer.colorize("●", colorizer.YELLOW)
-            path.contains(position) -> colorizer.colorize("●", colorizer.GREEN)
-            map[position] is PATH -> "."
-            else -> throw Error("not possible")
-        }
-    }
-
-    fun getPathToOxygen(): List<Position> {
-        val path = mutableListOf(Position(0, 0))
-        var position = Position(0, 0)
-
-        while (position != robot.position) {
-            position = position.getNeighbors().maxBy { distances[it] ?: -1 }!!
-            path.add(position)
-        }
-
-        return path
-    }
+    fun getPathToOxygen() = tileMap.getPathToOxygen()
 
     class OxygenFound : Error()
     class BackToStart : Error()
---
+++ src/day15/ShipMap.kt
@@ -0,0 +1,58 @@
+package day15
+
+class ShipMap(private val droid: RepairDroid) {
+    private val tileMap = HashMap<Position, Tile>()
+    private val distanceMap = HashMap<Position, Int>()
+
+    operator fun get(position: Position): Tile = tileMap[position]!!
+
+    operator fun set(position: Position, tile: Tile) {
+        tileMap[position] = tile
+
+        if (tile !is WALL)
+            distanceMap[position] = droid.steps
+    }
+
+    fun getPathToOxygen(): List<Position> {
+        var position = Position(0, 0)
+        val path = mutableListOf(position)
+
+        while (position != droid.position) {
+            position = position.getNeighbors().maxBy { distanceMap[it] ?: -1 }!!
+            path.add(position)
+        }
+
+        return path
+    }
+
+    override fun toString(): String {
+        val path = getPathToOxygen()
+        var result = ""
+        for (y in maxY() downTo minY()) {
+            for (x in minX()..maxX()) {
+                result += stringifyPosition(Position(x, y), path)
+            }
+            result += "\n"
+        }
+        return result
+    }
+
+    private fun stringifyPosition(position: Position, path: List<Position>): String {
+        val colorizer = Colorizer()
+        return when {
+            tileMap[position] == null -> " "
+            tileMap[position] is WALL -> colorizer.colorize("▯", colorizer.BLACK)
+            tileMap[position] is OXYGEN -> colorizer.colorize("●", colorizer.BLUE)
+            position == droid.position -> colorizer.colorize(droid.direction.toString(), colorizer.RED)
+            position == Position(0, 0) -> colorizer.colorize("●", colorizer.YELLOW)
+            path.contains(position) -> colorizer.colorize("●", colorizer.GREEN)
+            tileMap[position] is PATH -> "."
+            else -> throw Error("not possible")
+        }
+    }
+
+    private fun minX() = tileMap.keys.map { p -> p.x }.min()!!
+    private fun maxX() = tileMap.keys.map { p -> p.x }.max()!!
+    private fun minY() = tileMap.keys.map { p -> p.y }.min()!!
+    private fun maxY() = tileMap.keys.map { p -> p.y }.max()!!
+}
