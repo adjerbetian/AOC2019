181465faa7c654127ace83f81e24262ec3999837
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-12-31 00:46:24 +0100

    day16 - pretty print

--- src/day18/ProgressPrinter.kt
+++ src/day18/ProgressPrinter.kt
@@ -16,11 +16,17 @@ class ProgressPrinter(private val vault: Vault) {
     private fun printProgress() {
         if (Date().time - lastLog > LOG_INTERVAL) {
             counters.entries.forEach { println("${it.key} : ${prettyInt(it.value)}") }
-            println("best path length ${vault.bestPathLength}")
+            println("best path length: ${vault.bestPathLength}")
             println("---")
             lastLog = Date().time
         }
     }
 
-    private fun prettyInt(n: Long) = n.toString().reversed().split("000").joinToString("000 ").reversed()
+    private fun prettyInt(n: Long): String {
+        return n.toString()
+            .reversed()
+            .map { it.toString() }
+            .reduce { acc, s -> if (acc.length % 4 == 3) "$acc $s" else acc + s }
+            .reversed()
+    }
 }
